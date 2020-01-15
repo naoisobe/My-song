03ec54eed61568ba148d516f2b7d850f7c0f85a2
時原陸 <roy0817@Tokinomakku.local>
2020-01-15 16:12:38 +0900

    音声投稿機能の実装

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,4 +7,16 @@ class SongsController < ApplicationController
 
   def edit
   end
+
+  def new 
+    @song = Song.new
+  end 
+
+  def create
+  end
+
+  private
+  def song_params
+    params.require.(:song).permit(:title, :description, :thumbnail, :voice)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ApplicationRecord
+  mount_uploader :thumbnail, ThumbnailUploader
+  mount_uploader :voice, AudioFileUploader
 end
