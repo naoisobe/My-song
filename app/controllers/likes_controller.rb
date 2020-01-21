ff8ce558f4bf10d80a6b6739e7b24eeb4863da89
時原陸 <roy0817@Tokinomakku.local>
2020-01-21 17:33:24 +0900

    いいね機能非同期化途中

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,14 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new(song_id: params[:like][:song_id], user_id: current_user.id)
-    @like.save
-    redirect_back(fallback_location: root_path)
+    @like = Like.create(song_id: params[:id], user_id: current_user.id)
+    @song = Song.find(params[:id])
+    @like = Like.new
   end
 
   def destroy
     @like = Like.find_by(song_id: params[:id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    @song = Song.find(params[:id])
+    @like = Like.new
   end
 end
