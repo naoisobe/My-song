9d4a7fac40a757a17a3f34e5b75fbd66c6b9f4e3
時原陸 <roy0817@Tokinomakku.local>
2020-01-23 20:03:19 +0900

    branch切り替え

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show edit update]
   resources :comments, only: %i[create destroy]
   resources :likes, only: %i[create destroy]
-  resources :relationships, %i[:create destroy]
-  get 'follows/:id' => 'relationships#follows', as: :follows
-  get 'followers/:id' => 'relationships#followers', as: :followers
+  resources :relationships, only: %i[:create destroy]
+  get 'follows/:id' => 'relationships#follows', as: 'follows'
+  get 'followers/:id' => 'relationships#followers', as: 'followers'
   get 'songs/my_list/:id' => 'songs#my_list', as: 'song_my_list'
   get 'songs/search' => 'songs#search', as: 'songs_search'
   resources :songs
