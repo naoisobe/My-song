262a31ecf1635f34ceae477bd5a00ba27f71ad35
時原陸 <roy0817@Tokinomakku.local>
2020-01-28 11:21:43 +0900

    follow機能のpull

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
-//= require activestorage
 //= require_tree .
-//= require jquery3
+//= require activestorage
 //= require popper
 //= require bootstrap-sprockets
-//= require jquery  
-//= require jquery_ujs
 //= require audiojs
 
 // < !--audiojsの初期化 -->
@@ -25,12 +23,134 @@ audiojs.events.ready(function () {
   var as = audiojs.createAll();
 });
 
-$(document).on('ready', function () {
-  $(".full-screen-o").slick({
-    dots: true, // ドットインジケーターの表示
-    autoplay: true, // 自動再生を設定
-    autoplaySpeed: 2000, // 自動再生のスピード（ミリ秒単位）
-    speed: 1000, // スライド/フェードアニメーションの速度を設定
-    infinite: true // スライドのループを有効にするか
+// for html
+const downloadLink = document.getElementById('download');
+const stopButton = document.getElementById('stop');
+
+// for audio
+let audio_sample_rate = null;
+let scriptProcessor = null;
+let audioContext = null;
+
+// audio data
+let audioData = [];
+let bufferSize = 1024;
+
+let saveAudio = function () {
+  downloadLink.href = exportMP3(audioData);
+  downloadLink.download = 'test.mp3';
+  downloadLink.click();
+
+  audioContext.close().then(function () {
+    stopButton.setAttribute('disabled', 'disabled');
   });
+}
+
+// export WAV from audio float data
+let exportMP3 = function (audioData) {
+
+  let encodeWAV = function (samples, sampleRate) {
+    let buffer = new ArrayBuffer(44 + samples.length * 2);
+    let view = new DataView(buffer);
+
+    let writeString = function (view, offset, string) {
+      for (let i = 0; i < string.length; i++) {
+        view.setUint8(offset + i, string.charCodeAt(i));
+      }
+    };
+
+    let floatTo16BitPCM = function (output, offset, input) {
+      for (let i = 0; i < input.length; i++ , offset += 2) {
+        let s = Math.max(-1, Math.min(1, input[i]));
+        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+      }
+    };
+
+    writeString(view, 0, 'RIFF');  // RIFFヘッダ
+    view.setUint32(4, 32 + samples.length * 2, true); // これ以降のファイルサイズ
+    writeString(view, 8, 'WAVE'); // WAVEヘッダ
+    writeString(view, 12, 'fmt '); // fmtチャンク
+    view.setUint32(16, 16, true); // fmtチャンクのバイト数
+    view.setUint16(20, 1, true); // フォーマットID
+    view.setUint16(22, 1, true); // チャンネル数
+    view.setUint32(24, sampleRate, true); // サンプリングレート
+    view.setUint32(28, sampleRate * 2, true); // データ速度
+    view.setUint16(32, 2, true); // ブロックサイズ
+    view.setUint16(34, 16, true); // サンプルあたりのビット数
+    writeString(view, 36, 'data'); // dataチャンク
+    view.setUint32(40, samples.length * 2, true); // 波形データのバイト数
+    floatTo16BitPCM(view, 44, samples); // 波形データ
+
+    return view;
+  };
+
+  let mergeBuffers = function (audioData) {
+    let sampleLength = 0;
+    for (let i = 0; i < audioData.length; i++) {
+      sampleLength += audioData[i].length;
+    }
+    let samples = new Float32Array(sampleLength);
+    let sampleIdx = 0;
+    for (let i = 0; i < audioData.length; i++) {
+      for (let j = 0; j < audioData[i].length; j++) {
+        samples[sampleIdx] = audioData[i][j];
+        sampleIdx++;
+      }
+    }
+    return samples;
+  };
+
+  let dataview = encodeMP3(mergeBuffers(audioData), audio_sample_rate);
+  let audioBlob = new Blob([dataview], { type: 'audio/mp3' });
+  console.log(dataview);
+
+  let myURL = window.URL || window.webkitURL;
+  let url = myURL.createObjectURL(audioBlob);
+  return url;
+};
+
+// stop button
+stopButton.addEventListener('click', function () {
+  saveAudio();
+  console.log('saved mp3');
 });
+
+// save audio data
+var onAudioProcess = function (e) {
+  var input = e.inputBuffer.getChannelData(0);
+  var bufferData = new Float32Array(bufferSize);
+  for (var i = 0; i < bufferSize; i++) {
+    bufferData[i] = input[i];
+  }
+
+  audioData.push(bufferData);
+};
+
+// getusermedia
+let handleSuccess = function (stream) {
+  audioContext = new AudioContext();
+  audio_sample_rate = audioContext.sampleRate;
+  console.log(audio_sample_rate);
+  scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
+  var mediastreamsource = audioContext.createMediaStreamSource(stream);
+  mediastreamsource.connect(scriptProcessor);
+  scriptProcessor.onaudioprocess = onAudioProcess;
+  scriptProcessor.connect(audioContext.destination);
+
+  console.log('record start?');
+
+  // when time passed without pushing the stop button
+  setTimeout(function () {
+    console.log("10 sec");
+    if (stopButton.disabled == false) {
+      saveAudio();
+      console.log("saved audio");
+    }
+  }, 100000);
+};
+
+// getUserMedia
+navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+  .then(handleSuccess);
+
+
