bf03c6d6a7d95dac54e2dccbaa0422c1fe12d792
時原陸 <roy0817@Tokinomakku.local>
2020-02-03 11:38:00 +0900

    インストラクター用マイページ作成

--- app/controllers/instructor_controller.rb
+++ app/controllers/instructor_controller.rb
@@ -7,8 +7,24 @@ class InstructorController < ApplicationController
     @instructor = Instructor.find(params[:id])
   end
 
+  def update
+    @instructor = Instructor.find(params[:id])
+    if @instructor.update(instructor_params)
+      redirect_to instructor_path(@instructor)
+    else
+      render 'edit'
+    end
+  end
+
   def ms_list
     @practice = Practice.all
     @instructor = Instructor.find(params[:id])
   end
+
+  private
+  
+  def instructor_params
+    params.require(:instructor).permit(:name, :description, :profile_image)
+  end
 end
+
