c9089ddccf5836216438585e3cafe0401761877c
時原陸 <roy0817@Tokinomakku.local>
2020-02-07 11:47:45 +0900

    プロフィール画像更新時のプレビュー機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,3 +38,18 @@ document.addEventListener(
   },
   false
 );
+// プロフィール画像プレビュー
+$(function () {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#profile_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#file_upload").change(function () {
+    readURL(this);
+  });
+});
