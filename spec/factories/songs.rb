bf4d997bd55063ae182309c1b292667865be2674
時原陸 <roy0817@Tokinomakku.local>
2020-02-08 18:39:27 +0900

    アップロード日のカラーを見やすく変更

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     title { "MySong" }
     description { "MyText" }
     user_id { 1 }
-    voice { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.mp3')) }
+    voice { Rack::Test::AudioFileUploader.new(File.join(Rails.root, 'spec/fixtures/test.mp3')) }
   end
 end
