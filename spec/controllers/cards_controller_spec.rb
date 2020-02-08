b1ee808cf192e4300d6b719e422d7a085b7a3555
時原陸 <roy0817@Tokinomakku.local>
2020-02-09 00:52:14 +0900

    cardsコントローラーのテスト追加

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe CardsController, type: :controller do
-  
+  describe "createアクション" do
+    context "有料会員の場合" do
+
+      before do
+        @user = FactoryBot.create(:user, member_status: 1 )
+      end
+
+      it "レスポンスのステータスが[302（失敗)」になる" do
+        sign_in @user
+        get :create
+        expect(response).to have_http_status "302"
+      end
+
+      it "投稿一覧ページに遷移する" do
+        sign_in @user
+        get :create
+        expect(response).to redirect_to songs_path
+      end
+    end
+
+    context "未ログイン時" do
+      
+      it "レスポンスのステータスが[302（失敗)」になる" do
+        get :create
+        expect(response).to have_http_status "302"
+      end
+
+      it "サインインページに遷移する" do
+        get :create
+        expect(response).to redirect_to create_user_session_path
+      end
+    end
+  end
+
+  describe "createアクション" do
+    context "有料会員がログインしていた場合" do
+
+      before do
+        @user = FactoryBot.create(:user, member_status: 1 )
+      end
+
+      it "レスポンスのステータスが[302（失敗)」になる" do
+        sign_in @user
+        post :create
+        expect(response).to have_http_status "302"
+      end
+
+      it "投稿一覧ページに遷移する" do
+        sign_in @user
+        post :create
+        expect(response).to redirect_to songs_path
+      end
+    end
+
+    context "未ログイン時" do
+      
+      it "レスポンスのステータスが[302（失敗)」になる" do
+        post :create
+        expect(response).to have_http_status "302"
+      end
+
+      it "サインインページに遷移する" do
+        post :create
+        expect(response).to redirect_to create_user_session_path
+      end
+    end
+  end
 end
