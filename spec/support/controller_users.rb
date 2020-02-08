9343e6debe92836aa50089d59577e1eba3793ed0
時原陸 <roy0817@Tokinomakku.local>
2020-02-08 10:56:36 +0900

    userモデルのテストコード記述

---
+++ spec/support/controller_users.rb
@@ -0,0 +1,7 @@
+module ControllerUsers
+  def login_user(user)
+    controller.stub(:authenticate_user!).and_return true
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+end
