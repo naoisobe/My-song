1dd4ed9dc1447da2f701f80e9226f19cb2d9f4bf
時原陸 <roy0817@Tokinomakku.local>
2020-02-08 15:22:41 +0900

    動画ファイルのvalidationが機能するよう修正

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -38,7 +38,7 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
+  def extension_whitelist
     %w(mp4)
   end
 
