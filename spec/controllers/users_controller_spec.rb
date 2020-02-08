7a1e251714e0488f399ec95dd49ccbe53d026ab3
時原陸 <roy0817@Tokinomakku.local>
2020-02-09 00:22:20 +0900

    userコントローラのテスト追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,29 +3,44 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "updateアクション" do
+    context "他のuserを編集しようとした場合(ログイン時)"
+
     before do
+      @user = FactoryBot.create(:user)
+      @other_user = FactoryBot.create(:other_user)
     end
-      
+
+    it "他人のuser情報を更新することができない" do
+      user_params = FactoryBot.attributes_for(:user, name: "ChangeName")
+      sign_in @user
+      patch :update, params: { id: @other_user.id, user: user_params }
+      expect(@other_user.reload.name).to eq "testtaro"
     end
-    it "returns http success" do
-      user = FactoryBot.create(:correct_user)
-      sign_in user
-      g
+
+    it "投稿一覧ページにリダイレクトする" do
+      user_params = FactoryBot.attributes_for(:user)
+      sign_in @user
+      patch :update, params: { id: @other_user.id, user: user_params }
+      expect(response).to redirect_to songs_path
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  context "未ログイン時" do
+
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    it "レスポンスのステータスが[302（失敗)」になる" do
+      user_params = FactoryBot.attributes_for(:user)
+      patch :update, params: { id: @user.id, user: user_params }
+      expect(response).to have_http_status "302"
+    end
+
+    it "サインイン画面にリダイレクトする" do
+      user_params = FactoryBot.attributes_for(:user)
+      patch :update, params: { id: @user.id, user: user_params }
+      expect(response).to redirect_to new_user_session_path
     end
   end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
