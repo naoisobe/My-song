1bcc8a9fb5d959a1200a36f6a2717791787fb202
時原陸 <roy0817@Tokinomakku.local>
2020-02-11 19:15:31 +0900

    有料会員解約機能実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,17 +11,26 @@ class CardsController < ApplicationController
       customer = Payjp::Customer.create(
         card: params['payjp-token']
       )
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to songs_path
-      else
-        redirect_to new_card_path
-      end
-      Payjp::Subscription.create(
+
+      subscription = Payjp::Subscription.create(
         customer: customer,
         plan: 'pln_ab834b1d815833e30ca9dea71131'
       )
+
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, subscription_id: subscription.id, card_id: customer.default_card)
+      @card.save
+      redirect_to songs_path
     end
     current_user.update(member_status: 1)
+  end
+
+  def destroy
+    Payjp.api_key = 'sk_test_dadbfe665e6cc6dd9cad299f'
+    if card = Card.find_by(user_id: current_user.id)
+      subscription = Payjp::Subscription.retrieve(card.subscription_id)
+      subscription.cancel
+      current_user.update(member_status: 0)
+      redirect_to songs_path
+    end
   end
 end
