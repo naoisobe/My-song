0ff083441fa54728301f6e99dcc4d9d0c6a5f8c5
時原陸 <roy0817@Tokinomakku.local>
2020-02-12 15:27:48 +0900

    modalでのファイル選択画面後にdropzoneが表示されるよう修正

--- app/assets/javascripts/modal-new.js
+++ app/assets/javascripts/modal-new.js
@@ -1,9 +1,17 @@
 window.onload = function () {
-  $('#back').fadeIn();
+  $('.back').fadeIn();
   document.getElementById('next').onclick = function () {
-    $('#back').fadeOut();
+    $('.back').fadeOut();
   }
 };
 $('#next').on('click', function () {
   $('.show-back').show();
+  $('.back').addClass('new-back');
+  $('div').removeClass('back');
+  $('.modal-box').removeAttr('id');
+  $('.new-song-btn').remove();
+  $('.new-back').css({
+    background: 'whitesmoke',
+    marginBottom: '10px',
+  });
 });
