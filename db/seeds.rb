4868759ac6d894d61487fa70540af85e862c8c42
時原陸 <roy0817@Tokinomakku.local>
2020-02-13 18:25:27 +0900

    seedへのデモデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,46 @@ User.create!(
   password: "testpass",
   name: "ゲスト"
 )
+
+User.create!
+20.times do |n|
+  User.create!(
+  name: Faker::Name.name,
+  email: Faker::Internet.email,
+  password: "password"
+  )
+end
+
+15.times do |i|
+  Like.create!(
+    song_id: 1,
+    user_id: i + 1
+  )
+end
+
+20.times do |i|
+  Like.create!(
+    song_id: 2,
+    user_id: i + 1
+  )
+end
+
+25.times do |i|
+  Like.create!(
+    song_id: 3,
+    user_id: i + 1
+  )
+end
+
+10.times do |i|
+  Relationship.create!(
+    user_id: 1
+    follow_id: i + 2
+  )
+
+10.times do |i|
+  Relationship.create!(
+    user_id: i + 2
+    follow_id: 1
+  )
+
